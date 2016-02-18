2ad2f0261cec50abb35c952c687c78dddce87bb8
TAKEO <mr.thetakeo@gmail.com>
2016-02-19 00:16:56 +0800

    1st commit

---
+++ pixivtozip/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace PixivToZip
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PixivHelper pixivHelper;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            viewLogOut();
+            loadSetting();
+        }
+
+        private void viewLogOut()
+        {
+            tbUserId.IsEnabled = true;
+            tbPassword.IsEnabled = true;
+            btnLogIn.IsEnabled = true;
+
+            tbId.IsEnabled = false;
+            tbFolder.IsEnabled = false;
+            btnFolder.IsEnabled = false;
+            cbZip.IsEnabled = false;
+            btnDownload.IsEnabled = false;
+        }
+
+        private void viewLogIn()
+        {
+            tbUserId.IsEnabled = false;
+            tbPassword.IsEnabled = false;
+            btnLogIn.IsEnabled = false;
+
+            tbId.IsEnabled = true;
+            tbFolder.IsEnabled = true;
+            btnFolder.IsEnabled = true;
+            cbZip.IsEnabled = true;
+            btnDownload.IsEnabled = true;
+        }
+
+        private void disableAllContext()
+        {
+            tbUserId.IsEnabled = false;
+            tbPassword.IsEnabled = false;
+            btnLogIn.IsEnabled = false;
+
+            tbId.IsEnabled = false;
+            tbFolder.IsEnabled = false;
+            btnFolder.IsEnabled = false;
+            cbZip.IsEnabled = false;
+            btnDownload.IsEnabled = false;
+        }
+
+        private void saveSetting()
+        {
+            PixivToZip.Properties.Settings.Default.UserId = tbUserId.Text;
+            PixivToZip.Properties.Settings.Default.Password = tbPassword.Text;
+            PixivToZip.Properties.Settings.Default.Folder = tbFolder.Text;
+            PixivToZip.Properties.Settings.Default.IllustId = tbId.Text;
+            PixivToZip.Properties.Settings.Default.IsZip = (cbZip.IsChecked == true);
+
+            PixivToZip.Properties.Settings.Default.Save();
+        }
+
+        private void loadSetting()
+        {
+            tbUserId.Text = PixivToZip.Properties.Settings.Default.UserId;
+            tbPassword.Text = PixivToZip.Properties.Settings.Default.Password;
+            tbFolder.Text = PixivToZip.Properties.Settings.Default.Folder;
+            tbId.Text = PixivToZip.Properties.Settings.Default.IllustId;
+            cbZip.IsChecked = PixivToZip.Properties.Settings.Default.IsZip;
+        }
+
+        private async void btnLogIn_Click(object sender, RoutedEventArgs e)
+        {
+            pixivHelper = new PixivHelper();
+            bool result = await pixivHelper.logIn(tbUserId.Text, tbPassword.Text);
+
+            if (result)
+            {
+                System.Windows.MessageBox.Show("ログイン成功");
+                saveSetting();
+                viewLogIn();
+            }
+            else { System.Windows.MessageBox.Show("ログイン失敗"); }
+        }
+
+        private async void btnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            disableAllContext();
+
+            string folderPath = tbFolder.Text;
+
+            string dirPath = await pixivHelper.DownloadPicturesAsync(tbId.Text, folderPath);
+
+            string zipName = dirPath.Split('\\').Last() + ".zip";
+
+            if(cbZip.IsChecked==true)
+            {
+                ZipFile.CreateFromDirectory(dirPath, folderPath + @"\" + zipName);
+                Directory.Delete(dirPath,true);
+            }
+
+            saveSetting();
+
+            viewLogIn();
+        }
+
+        private void btnFolder_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tbFolder.Text = fbd.SelectedPath;
+                saveSetting();
+            }
+        }
+    }
+}
---
+++ pixivtozip/PixivHelper.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Net;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+
+namespace PixivToZip
+{
+    public class PixivHelper
+    {
+        OAuth oauth;
+
+        public async Task<bool> logIn(string username, string password)
+        {
+            oauth = new OAuth();
+            var task = oauth.authAsync(username, password);
+
+            bool result = false;
+            try
+            { result = await task; }
+            catch
+            { return false; }
+
+            return result;
+        }
+
+        public async Task<string> DownloadPicturesAsync(string id, string dir)
+        {
+            string folderName = "[" + await getPicturesUser(id) + "]" + await getPicturesTitle(id);
+
+            string path = dir + @"\" + folderName;
+
+            foreach (var url in await getPictures(id))
+            {
+                await downloadFileAsync(path, url);
+            }
+
+            return path;
+        }
+
+        private async Task<string> getPicturesTitle(string id)
+        {
+            Task<string> task = Task.Run(() =>
+            {
+                var json = illust_work(id);
+
+                if (json == null) return null;
+
+                List<string> result = new List<string>();
+
+                return json.Value<JArray>("response")[0]["title"].ToString();
+            });
+
+            return await task;
+        }
+
+        private async Task<string> getPicturesUser(string id)
+        {
+            Task<string> task = Task.Run(() =>
+            {
+                var json = illust_work(id);
+
+                if (json == null) return null;
+
+                List<string> result = new List<string>();
+
+                return json.Value<JArray>("response")[0]["user"]["name"].ToString();
+            });
+
+            return await task;
+        }
+
+        private async Task<List<string>> getPictures(string id)
+        {
+            Task<List<string>> task = Task.Run(() => 
+            {
+                var json = illust_work(id);
+
+                if (json == null) return null;
+
+                List<string> result = new List<string>();
+
+                foreach (JObject response in json.Value<JArray>("response"))
+                {
+                    if (!response["metadata"].HasValues)//illust
+                    { result.Add(response["image_urls"]["large"].ToString()); }
+                    else
+                    {
+                        if (!(bool)response["is_manga"])//ugoira
+                        {
+                            result.Add(response["image_urls"]["large"].ToString());
+                            result.Add(response["metadata"]["zip_urls"]["ugoira600x600"].ToString());
+                        }
+                        else// manga
+                        {
+                            foreach (JObject image in response["metadata"]["pages"].Value<JArray>())
+                            {
+                                result.Add(image["image_urls"]["large"].ToString());
+                            }
+                        }
+                    }
+                }
+                return result;
+            });
+
+            return await task;
+        }
+
+        private JObject illust_work(string illust_id)
+        {
+            return illust_workAsync(illust_id).Result;
+        }
+
+        private async Task<JObject> illust_workAsync(string illust_id)
+        {
+            string url = ("https://public-api.secure.pixiv.net/v1/works/" + illust_id + ".json");
+            var parameters = new Dictionary<string, object>(){
+                   {"image_sizes", "px_128x128,small,medium,large,px_480mw" },
+                   { "include_stats","true" }
+            };
+
+            var task = oauth.HttpGetAsync(url, parameters);
+            System.Net.Http.HttpResponseMessage message = null;
+
+            message = await task;
+
+            if (!message.IsSuccessStatusCode)
+            { return null; }
+            return JObject.Parse(message.Content.ReadAsStringAsync().Result);
+        }
+
+        private async Task<string> downloadFileAsync(string strPathName, string strUrl)
+        {
+            var task = oauth.HttpGetStreamAsync(null, strUrl, null);
+            int CompletedLength = 0;
+
+            string[] split = strUrl.Split('/');
+            string filename = split[split.Length - 1];
+
+            string fileRoute = strPathName + '/' + filename;
+            if (!Directory.Exists(strPathName))
+            { Directory.CreateDirectory(strPathName); }
+            if (File.Exists(fileRoute))
+            { File.Delete(fileRoute); }
+
+            using (FileStream FStream = new FileStream(fileRoute, FileMode.Create))
+            using (Stream myStream = await task)
+            {
+                byte[] btContent = new byte[1024];
+
+                await Task.Run(() =>
+                {
+                    while ((CompletedLength = myStream.Read(btContent, 0, 1024)) > 0)
+                    {
+                        FStream.Write(btContent, 0, CompletedLength);
+                    }
+                });
+
+                return fileRoute;
+            }
+        }
+    }
+}
